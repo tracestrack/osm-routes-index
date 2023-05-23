a901a223ef96e5df0d70875d165a62c9d727113c
strongwillow <clear.tsai@gmail.com>
2023-05-23 10:56:02 +0000

    update db 05/23

---
+++ generator/update.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+
+for sql in (ls *.sql)
+    sudo -u postgres psql -d gis -f $sql > $sql.results
+    set dname (echo $sql | sed 's/dump_//'| sed 's/\.sql//')
+    echo $dname
+    ./create_json_routes_db.js $sql.results $dname
+end
